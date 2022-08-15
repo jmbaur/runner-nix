1f2870aa7bddf2cea221dd45ae1e64320fd1972e
Jared Baur <jaredbaur@fastmail.com>
2022-08-15 11:40:03 -0700

    Reformat

--- packages.nix
+++ packages.nix
@@ -7,32 +7,31 @@ inputs: with inputs; flake-utils.lib.eachDefaultSystemMap (system:
   in
   {
     default = pkgs.runner-nix;
-    hello-test = pkgs.nixosTest
-      {
-        name = "hello";
-        nodes = {
-          runner = { pkgs, ... }: {
-            imports = [ self.nixosModules.default ];
-            services.runner = {
-              enable = true;
-              runs.hello = {
-                listenAddresses = [ "8000" "8001" ];
-                adapter = "none";
-                command = "${pkgs.hello}/bin/hello";
-                # Enforces a maximum of 2 hits per minute.
-                limitIntervalMinutes = 1;
-                limit = 2;
-              };
+    hello-test = pkgs.nixosTest {
+      name = "hello";
+      nodes = {
+        runner = { pkgs, ... }: {
+          imports = [ self.nixosModules.default ];
+          services.runner = {
+            enable = true;
+            runs.hello = {
+              listenAddresses = [ "8000" "8001" ];
+              adapter = "none";
+              command = "${pkgs.hello}/bin/hello";
+              # Enforces a maximum of 2 hits per minute.
+              limitIntervalMinutes = 1;
+              limit = 2;
             };
           };
         };
-        testScript = ''
-          runner.wait_for_unit("runner@hello.socket")
-          runner.succeed("curl -s localhost:8000")
-          runner.wait_for_console_text("Hello, world!")
-          runner.succeed("curl -s localhost:8001")
-          runner.wait_for_console_text("Hello, world!")
-          runner.fail("curl -s localhost:8000")
-        '';
       };
+      testScript = ''
+        runner.wait_for_unit("runner@hello.socket")
+        runner.succeed("curl -s localhost:8000")
+        runner.wait_for_console_text("Hello, world!")
+        runner.succeed("curl -s localhost:8001")
+        runner.wait_for_console_text("Hello, world!")
+        runner.fail("curl -s localhost:8000")
+      '';
+    };
   })
